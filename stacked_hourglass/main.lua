fe13ca6fd2607e2e2f125a5efe7c4f44dd4e9b71
alanwang93 <yifanwang1993@gmail.com>
2018-01-10 19:12:38 +0100

    add a function to produce(make up) h5 file for images without annotations

--- stacked_hourglass/main.lua
+++ stacked_hourglass/main.lua
@@ -6,7 +6,10 @@ paths.dofile('img.lua')
 -- Initialization
 --------------------------------------------------------------------------------
 
-if arg[1] == 'demo' or arg[1] == 'predict-test' then
+if arg[1] == 'mytest' then
+    a = loadAnnotations('mytest')
+
+elseif arg[1] == 'demo' or arg[1] == 'predict-test' then
     -- Test set annotations do not have ground truth part locations, but provide
     -- information about the location and scale of people in each image.
     a = loadAnnotations('test')
@@ -47,7 +50,7 @@ end
 --------------------------------------------------------------------------------
 -- Main loop
 --------------------------------------------------------------------------------
-
+-- Only 3 attributes are used: images, center, scale
 for i = 1,nsamples do
     -- Set up input image
     local im = image.load('images/' .. a['images'][idxs[i]])
